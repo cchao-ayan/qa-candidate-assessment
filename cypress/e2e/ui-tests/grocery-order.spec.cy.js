b884d2174a4f72ca5e1bed7c5205dabb0d37a2e0
Christian <cchaoayan@gmail.com>
2026-04-03 00:15:41 +0800

    added beforeEach

--- cypress/e2e/ui-tests/grocery-order.spec.cy.js
+++ cypress/e2e/ui-tests/grocery-order.spec.cy.js
@@ -1,13 +1,12 @@
 describe('Order Groceries', () => {
-  it('Visits Souce Demo website', () => {
+  beforeEach('Visits Source Demo website and logs in', () => {
     cy.visit('/');
+    cy.get('[data-test="username"]').type('standard_user');
+    cy.get('[data-test="password"]').type('secret_sauce');
+    cy.get('[data-test="login-button"]').click();
   });
-  it('Login successfully', () => {
-    cy.get('#user-name').type('standard_user');
-    cy.get('#password').type('secret_sauce');
-    cy.get('#login-button').click();
-  });
-  it('Add items to cart and checkout', () => {
+  it('should complete a grocery order end-to-end', () => {
+    // Add items to cart
     cy.get('[data-test="product-sort-container"]').select('Price (low to high)');
     cy.get('[data-test="inventory-item"]').first().within(() => {
       cy.get('[data-test="add-to-cart-sauce-labs-onesie"]').click();
@@ -18,17 +17,17 @@ describe('Order Groceries', () => {
     });
     cy.get('[data-test="shopping-cart-link"]').click();
     cy.get('[data-test="checkout"]').click();
-  });
-  it('Fill in checkout information and complete order', () => {
+
+    // Fill in checkout information
     cy.get('[data-test="firstName"]').type('test_first_name');
     cy.get('[data-test="lastName"]').type('test_lastname');
-    cy.get('[data-test="postalCode"]').type('test_postal_code')
+    cy.get('[data-test="postalCode"]').type('test_postal_code');
     cy.get('[data-test="continue"]').click();
-  });
-  it('Finish order and logout', () => {
+
+    // Finish order and logout
     cy.get('[data-test="finish"]').click();
     cy.get('[data-test="back-to-products"]').click();
     cy.get('#react-burger-menu-btn').click();
     cy.get('[data-test="logout-sidebar-link"]').click();
-  })
+  });
 })
