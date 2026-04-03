0c79160794f9d0404f57d9962ef903c831aa248a
Christian <cchaoayan@gmail.com>
2026-04-03 23:23:06 +0800

    added k6 options

--- cypress/e2e/performance-tests/k6/tests.js
+++ cypress/e2e/performance-tests/k6/tests.js
@@ -3,12 +3,22 @@ import { check, sleep } from 'k6';
 import { createOrder } from './scripts.js';
 
 export const options = {
-  vus: 1,          // virtual users
-  //duration: '10s',  // test duration
-  iterations: 1        // number of iterations per virtual user
+    //vus: 1,          // virtual users
+    //duration: '10s',  // test duration
+    //iterations: 1,        // number of iterations per virtual user
+
+    stages: [
+        { duration: '1m', target: 10 }, // ramp up to 10 users over 1 minute
+        { duration: '3m', target: 10 }, // stay at 10 users for 3 minutes
+        { duration: '1m', target: 0 },  // ramp down to 0 users over 1 minute
+    ],
+    thresholds: {
+        'http_req_duration': ['p(95)<500'], // 95% of requests should be below 500ms
+        'http_req_failed': ['rate<0.01'], // less than 1% of requests should fail
+    },
 };
 
 export default function () {
-  createOrder();
-  sleep(1);
+    createOrder();
+    sleep(1);
 }
