e7c87a4c665b24d3ca82396ad30691e0ab927c68
Christian <cchaoayan@gmail.com>
2026-04-03 08:45:44 +0800

    update k6 scripts

--- cypress/e2e/performance-tests/k6-test.js
+++ cypress/e2e/performance-tests/k6-test.js
@@ -6,18 +6,59 @@ export const options = {
   duration: '10s',  // test duration
 };
 
-export default function () {
+export function login() {
+  const response = http.post('https://simple-grocery-store-api.click/api-clients', JSON.stringify({
+    "username": "test_user",
+    "password": "test_password"
+  }), {
+    headers: {
+      "Content-Type": "application/json"
+    },
+  });
+  return response.json().accessToken;
+}
+
+export function getProducts() {
+  const response = http.get('https://simple-grocery-store-api.click/products');
+  return response.json().products[0].id;
+}
+
+export function createCart() {
+  const response = http.post('https://simple-grocery-store-api.click/carts', null, {
+    headers: {
+      "Content-Type": "application/json"
+    },
+  });
+  return response.json().cartId;
+}
+
+export function addItemToCart() {
+  const cartId = createCart();
+  const response = http.post(`https://simple-grocery-store-api.click/carts/${cartId}/items`, JSON.stringify({
+    "productId": getProducts()
+  }), {
+    headers: {
+      "Content-Type": "application/json"
+    },
+  });
+  return response.json().itemId;
+}
+
+export function createOrder() {
+  const accessToken = login();
+  const cartId = createCart();
+  addItemToCart();
   const url = 'https://simple-grocery-store-api.click/orders';
 
   const payload = JSON.stringify({
-    "cartId": "oFd2hATO1zDQPsEy0kZxX",
+    "cartId": cartId,
     "customerName": "Ella Nikolaus"
   });
 
   const params = {
     headers: {
-        "Authorization": "Bearer 69df154bea6db63fe073522ff55c3c2b0e0528c47ba2fe3b5792e22c4ba2f9c1",
-        "Content-Type": "application/json"
+      "Authorization": `Bearer ${accessToken}`,
+      "Content-Type": "application/json"
     },
   };
 
@@ -26,6 +67,9 @@ export default function () {
   check(response, {
     'status is 201': (r) => r.status === 201,
   });
+}
 
+export default function () {
+  createOrder();
   sleep(1);
 }
